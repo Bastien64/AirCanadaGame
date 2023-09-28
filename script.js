0f7c35ca644cd959d4d01d94e11a54484c47189b
Bastien VILLEGAS <95182085+Bastien64@users.noreply.github.com>
2023-09-28 12:27:38 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -100,32 +100,151 @@ window.addEventListener("scroll", function () {
     }
 });
 
+
+// Code pour thisWeek
 fetch('data.json')
     .then(response => response.json())
     .then(data => {
-        const tableBody = document.getElementById('table-body');
+        const thisWeekTableBody = document.getElementById('this-week-table-body');
         const thisWeekData = data.thisWeek;
-        
-        // Parcourir les données de points
+
+        // Parcourir les données de points pour thisWeek
         thisWeekData.points.forEach(item => {
-          const row = document.createElement('tr');
-        
-          // Trouver les données de temps, de vols et de bonus pour l'utilisateur actuel
-          const timeResult = thisWeekData.time.find(timeItem => timeItem.id === item.id).result;
-          const flightsResult = thisWeekData.flights.find(flightItem => flightItem.id === item.id).result;
-          const bonusResult = thisWeekData.bonus.find(bonusItem => bonusItem.id === item.id).result;
-        
-          // Ajouter les données à la ligne du tableau
-          row.innerHTML = `
+            const row = document.createElement('tr');
+
+            // Trouver les données de temps, de vols et de bonus pour l'utilisateur actuel
+            const timeResult = thisWeekData.time.find(timeItem => timeItem.id === item.id).result;
+            const flightsResult = thisWeekData.flights.find(flightItem => flightItem.id === item.id).result;
+            const bonusResult = thisWeekData.bonus.find(bonusItem => bonusItem.id === item.id).result;
+
+            // Ajouter les données à la ligne du tableau
+            row.innerHTML = `
+                <td>${item.username}</td>
+                <td>${item.result}</td>
+                <td>${timeResult}</td>
+                <td>${flightsResult}</td>
+                <td>${bonusResult}</td>
+            `;
+
+            // Ajouter la ligne du tableau au corps du tableau pour thisWeek
+            thisWeekTableBody.appendChild(row);
+        });
+    })
+    .catch(error => {
+        console.error('Une erreur s\'est produite pour thisWeek :', error);
+    });
+
+// Code pour thisMonth
+fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+        const thisMonthTableBody = document.getElementById('this-month-table-body');
+        const thisMonthData = data.thisMonth;
+
+        // Parcourir les données de points pour thisMonth
+        thisMonthData.points.forEach(item => {
+            const row = document.createElement('tr');
+
+            // Trouver les données de temps, de vols et de bonus pour l'utilisateur actuel
+            const timeResult = thisMonthData.time.find(timeItem => timeItem.id === item.id).result;
+            const flightsResult = thisMonthData.flights.find(flightItem => flightItem.id === item.id).result;
+            const bonusResult = thisMonthData.bonus.find(bonusItem => bonusItem.id === item.id).result;
+
+            // Ajouter les données à la ligne du tableau
+            row.innerHTML = `
+                <td>${item.username}</td>
+                <td>${item.result}</td>
+                <td>${timeResult}</td>
+                <td>${flightsResult}</td>
+                <td>${bonusResult}</td>
+            `;
+
+            // Ajouter la ligne du tableau au corps du tableau pour thisMonth
+            thisMonthTableBody.appendChild(row);
+        });
+    })
+    .catch(error => {
+        console.error('Une erreur s\'est produite pour thisMonth :', error);
+    });
+
+    // Code pour allTime
+fetch('data.json')
+.then(response => response.json())
+.then(data => {
+    const allTimeTableBody = document.getElementById('all-time-table-body');
+    const allTimeData = data.allTime;
+
+    // Parcourir les données de points pour allTime
+    allTimeData.points.forEach(item => {
+        const row = document.createElement('tr');
+
+        // Trouver les données de temps, de vols et de bonus pour l'utilisateur actuel
+        const timeResult = allTimeData.time.find(timeItem => timeItem.id === item.id).result;
+        const flightsResult = allTimeData.flights.find(flightItem => flightItem.id === item.id).result;
+        const bonusResult = allTimeData.bonus.find(bonusItem => bonusItem.id === item.id).result;
+
+        // Ajouter les données à la ligne du tableau
+        row.innerHTML = `
             <td>${item.username}</td>
             <td>${item.result}</td>
             <td>${timeResult}</td>
             <td>${flightsResult}</td>
             <td>${bonusResult}</td>
+        `;
+
+        // Ajouter la ligne du tableau au corps du tableau pour allTime
+        allTimeTableBody.appendChild(row);
+    });
+})
+.catch(error => {
+    console.error('Une erreur s\'est produite pour allTime :', error);
+});
+
+
+
+
+
+
+
+  // Charger les données JSON depuis le fichier "airplane.json"
+  fetch('airplane.json')
+  .then(response => response.json())
+  .then(data => {
+      const tableBody = document.getElementById('table-body');
+
+      // Parcourir les données JSON et les ajouter au tableau
+      data.forEach(item => {
+          const row = document.createElement('tr');
+          row.innerHTML = `
+              <td>${item.id}</td>
+              <td>${item.callsign}</td>
+              <td>${item['flight-number']}</td>
+              <td>${item.pax}</td>
+              <td>${item.cargo}</td>
+              <td>${item.route}</td>
+              <td>${item.network}</td>
+              <td>${item.currentLocation.altitude}</td>
+              <td>${item.currentLocation.heading}</td>
+              <td>${item.currentLocation.latitude}</td>
+              <td>${item.currentLocation.longitude}</td>
+              <td>${item.currentLocation.groundspeed}</td>
+              <td>${item.currentLocation.distance_remaining}</td>
+              <td>${item.currentLocation.time_remaining}</td>
+              <td>${item.currentLocation.distance_flown}</td>
+              <td>${item.currentLocation.departure_time}</td>
+              <td>${item.currentLocation.estimated_arrival_time}</td>
+              <td>${item.currentLocation.time_flown}</td>
+              <td>${item.aircraft.registration}</td>
+              <td>${item.aircraft.name}</td>
+              <td>${item.aircraft.code}</td>
+              <td>${item.aircraft.codename}</td>
+              <td>${item.departure.name}</td>
+              <td>${item.arrival.name}</td>
+              <td>${item.pilot.username}</td>
           `;
-        
-          // Ajouter la ligne du tableau au corps du tableau
           tableBody.appendChild(row);
-          console.log(data.thisWeek);
-        });
-    })
+      });
+  })
+  .catch(error => {
+      console.error('Une erreur s\'est produite :', error);
+  });
